d5bedf5a7c2e2bd1244d79769825a829e88949b3
rvjq <rvjq@cesar.school>
2024-11-28 18:45:37 -0300

    bugfix

--- src/modules/Login/controller/LoginController.ts
+++ src/modules/Login/controller/LoginController.ts
@@ -17,22 +17,6 @@ class LoginController {
                 throw new AppError(`${error.message}`, 400);
             }
 
-            const LoginResult = await this.loginUserService.login(req.body)
-
-            res.status(201).json(LoginResult)
-
-            
-
-        } catch (error: unknown) { 
-            if (error instanceof Error) { 
-                console.error('Error creating user:', error); 
-                res.status(409).json({ message: error.message }); 
-            } else { 
-                console.error('Unexpected error:', error); 
-                res.status(error.statusCode).json({ message: error}); } 
-            }
-    }
-}
             const loginResult = await this.loginService.login(req.body);
              res.status(200).json(loginResult);
         } catch (error) {
